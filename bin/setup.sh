c1bea3d2ed2b63ea328180836325a46a59a13cca
shinau21 <bandingandesa@gmail.com>
2023-05-22 13:53:30 +0700

    Modified again

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source .env
 
-sed "s/USERNAME/${MYSQL_USER}/g" sites/localhost/html/config/config.database.php
-sed "s/PASSWORD/${MYSQL_PASSWORD}/g" sites/localhost/html/config/config.database.php
-sed "s/DATABASE/${MYSQL_DATABASE}/g" sites/localhost/html/config/config.database.php
+sed -i "s/USERNAME/${MYSQL_USER}/g" sites/localhost/html/config/config.database.php
+sed -i "s/PASSWORD/${MYSQL_PASSWORD}/g" sites/localhost/html/config/config.database.php
+sed -i "s/DATABASE/${MYSQL_DATABASE}/g" sites/localhost/html/config/config.database.php
