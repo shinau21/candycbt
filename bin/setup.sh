16622ffc086dce90405a2c54644a0ee9596c43dc
Rafli Setiawan <shinau21@DESKTOP-HKTBPMU>
2023-05-22 13:49:06 +0700

    Modified Setup Script

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source .env
 
-sed "s/USERNAME/${MYSQL_USER}/g" /var/www/vhosts/localhost/html/config/config.database.php
-sed "s/PASSWORD/${MYSQL_PASSWORD}/g" /var/www/vhosts/localhost/html/config/config.database.php
-sed "s/DATABASE/${MYSQL_DATABASE}/g" /var/www/vhosts/localhost/html/config/config.database.php
+sed "s/USERNAME/${MYSQL_USER}/g" /sites/localhost/html/config/config.database.php
+sed "s/PASSWORD/${MYSQL_PASSWORD}/g" /sites/localhost/html/config/config.database.php
+sed "s/DATABASE/${MYSQL_DATABASE}/g" /sites/localhost/html/config/config.database.php
